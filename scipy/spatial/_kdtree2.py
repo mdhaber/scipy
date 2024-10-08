8af1362243e7c5ff5b3746814956eca870b4d280
Matt Haberland <mhaberla@calpoly.edu>
2024-10-07 17:43:56 -0700

    MAINT: spatial.kdtree2: refactor for clarity

--- scipy/spatial/_kdtree2.py
+++ scipy/spatial/_kdtree2.py
@@ -2,47 +2,40 @@ import numpy as np
 
 class kdtree2:
     def __init__(self, data):
-        n, k = data.shape
         self.data = data
-        self.children = np.full((n, 2), -1)
-        self.parent = np.full(n, -1)
-        self.axis = np.full(n, -1)
-        self.nodes = np.arange(n)
-        self.k = k
-        self.n = n
-        i_median = self.n // 2
-        self.nodes = np.argpartition(self.data[:, 0], i_median)
-        self.root = self.nodes[i_median]
-        self.axis[self.root] = 0
-        self.construct(self.nodes[:i_median], self.nodes[i_median+1:], self.root, 1)
+        self.n, self.k = data.shape
+        self.children = np.full((self.n, 2), -1)
+        self.axis = np.full(self.n, -1)
+        self.root = self.construct(np.arange(self.n), 0)
 
-    def construct(self, left, right, node, level):
-        d = level % self.k
+    def construct(self, indices, level):
+        n = len(indices)
+        if n == 0:
+            return -1
 
-        if len(left) > 1:
-            i_left = len(left) // 2
-            left[:] = left[np.argpartition(self.data[left, d], i_left)]
-            left_node = left[i_left]
-            self.axis[left_node] = d
-            self.construct(left[:i_left], left[i_left+1:], left_node, level + 1)
-            self.children[node, 0] = left_node
-        elif len(left) == 1:
-            self.axis[left[0]] = d
-            self.children[node, 0] = left[0]
+        axis = level % self.k
+        if n == 1:
+            node = indices[0]
+            self.axis[node] = axis
+            return node
 
-        if len(right) > 1:
-            i_right = len(right) // 2
-            right[:] = right[np.argpartition(self.data[right, d], i_right)]
-            right_node = right[i_right]
-            self.axis[right_node] = d
-            self.construct(right[:i_right], right[i_right+1:], right_node, level + 1)
-            self.children[node, 1] = right_node
-        elif len(right) == 1:
-            self.axis[right[0]] = d
-            self.children[node, 1] = right[0]
+        i = n // 2
+        indices = indices[np.argpartition(self.data[indices, axis], i)]
+
+        node = indices[i]
+        self.axis[node] = axis
+
+        # construct children
+        next_level = level + 1
+        left = self.construct(indices[:i], next_level)
+        right = self.construct(indices[i + 1:], next_level)
+        self.children[node] = left, right
+
+        return node
+
 
     def distance(self, point1, point2):
-        return np.sum((point1 - point2)**2)**0.5
+        return np.linalg.vector_norm(point1 - point2)
 
     def nearest_neighbor(self, point):
         node, distance = self._nearest_neighbor(point, self.root)
