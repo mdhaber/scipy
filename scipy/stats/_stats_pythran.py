a121598ae167bce7941608d2f0911cfd453e1261
Matt Haberland <mhaberla@calpoly.edu>
2025-01-29 18:28:46 -0800

    ENH: stats: add private bivariate normal implementation in Pythran

--- scipy/stats/_stats_pythran.py
+++ scipy/stats/_stats_pythran.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import ndtr as phid
 
 
 #pythran export _Aij(float[:,:], int, int)
@@ -209,3 +210,143 @@ def _poisson_binom(k, args, tp):
             cache[p] = np.cumsum(pmf) if tp=='cdf' else pmf
         out[i] = cache[p][k[i]]
     return out
+
+
+### Bivariate Normal
+#BVNU
+#  A function for computing bivariate normal probabilities.
+#  bvnu calculates the probability that x > dh and y > dk.
+#    parameters
+#      dh 1st lower integration limit
+#      dk 2nd lower integration limit
+#      r   correlation coefficient
+#  Example: p = bvnu( -3, -1, .35 )
+#  Note: to compute the probability that x < dh and y < dk,
+#        use bvnu( -dh, -dk, r ).
+#
+#BVN
+#  A function for computing bivariate normal probabilities.
+#  bvn calculates the probability that
+#    xl < x < xu and yl < y < yu,
+#  with correlation coefficient r.
+#   p = bvn( xl, xu, yl, yu, r )
+#
+#   Author
+#       Alan Genz
+#       Department of Mathematics
+#       Washington State University
+#       Pullman, Wa 99164-3113
+#       Email : alangenz@wsu.edu
+#
+#    This function is based on the method described by 
+#        Drezner, Z and G.O. Wesolowsky, (1989),
+#        On the computation of the bivariate normal inegral,
+#        Journal of Statist. Comput. Simul. 35, pp. 101-107,
+#    with major modifications for double precision, for |r| close to 1,
+#    and for Matlab by Alan Genz. Minor bug modifications 7/98, 2/10.
+#
+#
+# Copyright (C) 2013, Alan Genz,  All rights reserved.        
+# Translated by ChatGPT
+# https://chatgpt.com/share/679ad125-eee8-8010-b44f-5d771f0fa791
+
+
+# pythran export bvnu(float, float, float)
+def bvnu(dh, dk, r):
+    if dh == np.inf or dk == np.inf:
+        return 0
+    elif dh == -np.inf:
+        return 1 if dk == -np.inf else phid(-dk)
+    elif dk == -np.inf:
+        return phid(-dh)
+    elif r == 0:
+        return phid(-dh) * phid(-dk)
+    else:
+        tp = 2 * np.pi
+        h, k, hk = dh, dk, dh * dk
+        bvn = 0
+
+        if abs(r) < 0.3:
+            w = np.array([0.1713244923791705, 0.3607615730481384, 0.4679139345726904])
+            x = np.array([0.9324695142031522, 0.6612093864662647, 0.2386191860831970])
+        elif abs(r) < 0.75:
+            w = np.array([0.04717533638651177, 0.1069393259953183, 0.1600783285433464,
+                          0.2031674267230659, 0.2334925365383547, 0.2491470458134029])
+            x = np.array([0.9815606342467191, 0.9041172563704750, 0.7699026741943050,
+                          0.5873179542866171, 0.3678314989981802, 0.1252334085114692])
+        else:
+            w = np.array([0.01761400713915212, 0.04060142980038694, 0.06267204833410906,
+                          0.08327674157670475, 0.1019301198172404, 0.1181945319615184,
+                          0.1316886384491766, 0.1420961093183821, 0.1491729864726037,
+                          0.1527533871307259])
+            x = np.array([0.9931285991850949, 0.9639719272779138, 0.9122344282513259,
+                          0.8391169718222188, 0.7463319064601508, 0.6360536807265150,
+                          0.5108670019508271, 0.3737060887154196, 0.2277858511416451,
+                          0.07652652113349733])
+
+        w = np.concatenate((w, w))
+        x = np.concatenate((1 - x, 1 + x))
+
+        if abs(r) < 0.925:
+            hs = (h ** 2 + k ** 2) / 2
+            asr = np.arcsin(r) / 2
+            sn = np.sin(asr * x)
+            # bvn = np.exp((sn * hk - hs) / (1 - sn ** 2)) @ w
+            bvn = np.sum(np.exp((sn * hk - hs) / (1 - sn ** 2)) * w)
+            bvn = bvn * asr / tp + phid(-h) * phid(-k)
+        else:
+            if r < 0:
+                k, hk = -k, -hk
+
+            if abs(r) < 1:
+                as_ = 1 - r ** 2
+                a = np.sqrt(as_)
+                bs = (h - k) ** 2
+                asr = -(bs / as_ + hk) / 2
+                c = (4 - hk) / 8
+                d = (12 - hk) / 80
+
+                if asr > -100:
+                    bvn = a * np.exp(asr) * (1 - c * (bs - as_) * (1 - d * bs) / 3 + c * d * as_ ** 2)
+
+                if hk > -100:
+                    b = np.sqrt(bs)
+                    sp = np.sqrt(tp) * phid(-b / a)
+                    bvn -= np.exp(-hk / 2) * sp * b * (1 - c * bs * (1 - d * bs) / 3)
+
+                a /= 2
+                xs = (a * x) ** 2
+                asr = -(bs / xs + hk) / 2
+                ix = asr > -100
+                xs = xs[ix]
+                sp = (1 + c * xs * (1 + 5 * d * xs))
+                rs = np.sqrt(1 - xs)
+                ep = np.exp(-(hk / 2) * xs / (1 + rs) ** 2) / rs
+                # bvn = (a * ((np.exp(asr[ix]) * (sp - ep)) @ w[ix]) - bvn) / tp
+                bvn = (a * np.sum((np.exp(asr[ix]) * (sp - ep)) * w[ix]) - bvn) / tp
+
+            if r > 0:
+                bvn += phid(-max(h, k))
+            elif h >= k:
+                bvn = -bvn
+            else:
+                L = phid(k) - phid(h) if h < 0 else phid(-h) - phid(-k)
+                bvn = L - bvn
+
+        return max(0, min(1, bvn))
+
+
+# pythran export bvn(float, float, float, float, float)
+def bvn(xl, xu, yl, yu, r):
+    p = bvnu(xl, yl, r) - bvnu(xu, yl, r) - bvnu(xl, yu, r) + bvnu(xu, yu, r)
+    return max(0, min(1, p))
+
+
+# def compute_bvn(x, cov):
+#     # Standardize x using the covariance matrix
+#     dh = x[0] / np.sqrt(cov[0, 0])  # Normalize x
+#     dk = x[1] / np.sqrt(cov[1, 1])  # Normalize y
+#     r = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])  # Compute correlation
+#
+#     # Call bvnu with the transformed variables
+#     return bvnu(-dh, -dk, r)
