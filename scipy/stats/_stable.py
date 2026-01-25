ff93d1630206e290774999371fd15d00542f84c5
Matt Haberland <matt.haberland@gmail.com>
2026-01-24 16:57:24 -0800

    MAINT: stats.Stable: reface more for concision and maintainability

--- scipy/stats/_stable.py
+++ scipy/stats/_stable.py
@@ -5,101 +5,92 @@ import scipy._lib.array_api_extra as xpx
 from scipy._lib._array_api import xp_promote
 
 
-def zeta(a, b):
-    return -b * np.tan(np.pi * a / 2)
+def zeta(a, b, *, a1):
+    if not a1:
+        return -b * np.tan(np.pi * a / 2)
+    else:
+        return np.zeros_like(a)
 
 
-def zetaa1(a, b):
-    return np.zeros_like(a)
+def th0(a, b, *, a1):
+    if not a1:
+        return 1/a * np.atan(b * np.tan(np.pi * a / 2))
+    else:
+        return np.full_like(a, np.pi / 2)
 
 
-def th0(a, b):
-    return 1 / a * np.atan(b * np.tan(np.pi * a / 2))
+def c1(a, b, *, a1):
+    if not a1:
+        return np.where(a < 1, 1/np.pi * (np.pi/2 - th0(a, b, a1=a1)), 1.)
+    else:
+        return np.zeros_like(a)
 
 
-def th0a1(a, b):
-    return np.full_like(a, np.pi/2)
+def V(th, a, b, *, a1):
+    if not a1:
+        th0_ = th0(a, b, a1=a1)
+        t1 = (np.cos(a * th0_))**(1 / (a - 1))
+        t2 = (np.cos(th) / np.sin(a * (th0_ + th)))**(a / (a - 1))
+        t3 = np.cos(a*th0_ + (a - 1)*th) / np.cos(th)
+        return t1 * t2 * t3
+    else:
+        return 2 / np.pi * (np.pi/2 + b*th) / np.cos(th) * np.exp(
+            1/b * (np.pi/2 + b*th) * np.tan(th))
 
 
-def c1(a, b):
-    return np.where(a < 1, 1/np.pi * (np.pi/2 - th0(a, b)), 1.)
+def c2(x, a, b, *, a1):
+    if not a1:
+        return a / (np.pi * np.abs(a - 1) * (x - zeta(a, b, a1=a1)))
+    else:
+        return 1 / (2 * np.abs(b))
 
 
-def c1a1(a, b):
-    return np.zeros_like(a)
+def c3(a, *, a1):
+    if not a1:
+        return np.sign(1 - a) / np.pi
+    else:
+        return np.full_like(a, 1 / np.pi)
 
 
-def V(th, a, b):
-    th0_ = th0(a, b)
-    t1 = (np.cos(a * th0_))**(1 / (a - 1))
-    t2 = (np.cos(th) / np.sin(a * (th0_ + th)))**(a / (a - 1))
-    t3 = np.cos(a*th0_ + (a - 1)*th) / np.cos(th)
-    return t1 * t2 * t3
-
-
-def Va1(th, a, b):
-    return 2/np.pi * (np.pi/2 + b*th) / np.cos(th) * np.exp(
-        1/b * (np.pi/2 + b*th) * np.tan(th))
-
-
-def c2(x, a, b):
-    return a / (np.pi * np.abs(a - 1) * (x - zeta(a, b)))
-
-
-def c2a1(x, a, b):
-    return 1 / (2 * np.abs(b))
-
-
-def c3(a):
-    return np.sign(1 - a) / np.pi
-
-
-def c3a1(a):
-    return np.full_like(a, 1 / np.pi)
-
-
-def g(th, x, a, b):
-    return (x - zeta(a, b)) ** (a / (a - 1)) * V(th, a, b)
-
-
-def ga1(th, x, a, b):
-    return np.exp(-np.pi / 2 * x / b) * Va1(th, 1, b)
+def g(th, x, a, b, *, a1):
+    if not a1:
+        return (x - zeta(a, b, a1=a1))**(a/(a - 1)) * V(th, a, b, a1=a1)
+    else:
+        return np.exp(-np.pi/2 * x/b) * V(th, 1, b, a1=a1)
 
 
 def Fi(x, a, b, *, a1):
-    g_fun, th0_fun, c1_fun, c3_fun = (ga1, th0a1, c1a1, c3a1) if a1 else (g, th0, c1, c3)
-
     def integrand(th, x, a, b):
-        return np.exp(-g_fun(th, x, a, b))
+        return np.exp(-g(th, x, a, b, a1=a1))
 
-    integral = integrate.tanhsinh(integrand, -th0_fun(a, b), np.pi / 2, args=(x, a, b)).integral
-    return c1_fun(a, b) + c3_fun(a) * integral
+    integral = integrate.tanhsinh(integrand, -th0(a, b, a1=a1), np.pi / 2, args=(x, a, b)).integral
+    return c1(a, b, a1=a1) + c3(a, a1=a1) * integral
 
 
 def fi(x, a, b, *, a1):
-    g_fun, th0_fun, c2_fun = (ga1, th0a1, c2a1) if a1 else (g, th0, c2)
-
     def integrand(th, x, a, b):
-        g_ = g_fun(th, x, a, b)
+        g_ = g(th, x, a, b, a1=a1)
         return g_ * np.exp(-g_)
 
-    bounds = (np.nextafter(-th0_fun(a, b), np.inf), np.nextafter(np.pi / 2, -np.inf))
+    # re-evaluate nextafter on the right
+    bounds = (np.nextafter(-th0(a, b, a1=a1), np.inf), np.pi / 2)
 
     # root-finding doesn't work in the left tail (|b| ~ 1)
-    # th2 = elementwise.find_root(lambda th, x, a, b: g(th, x, a, b) - 1,
+    # th2 = elementwise.find_root(lambda th, x, a, b: g(th, x, a, b, a1=a1) - 1,
     #                             bounds, args=(x, a, b))
     # integral1 = integrate.tanhsinh(integrand, bounds[0], th2.x, args=(x, a, b))
     # integral2 = integrate.tanhsinh(integrand, th2.x, np.pi/2, args=(x, a, b))
     # integral = integral1.integral + integral2.integral
 
     integral = integrate.tanhsinh(integrand, *bounds, args=(x, a, b)).integral
-    return c2_fun(x, a, b) * integral
+    return c2(x, a, b, a1=a1) * integral
 
 
 def F(x, a, b):
     x, a, b = xp_promote(x, a, b, force_floating=True, broadcast=True, xp=np)
     a1 = np.all(a == 1)
-    i = (b > 0) if a1 else (x >= zeta(a, b))
+    i = (b > 0) if a1 else (x >= zeta(a, b, a1=a1))
+    # ideally, a lazy-select function would let us handle a=1, b=0 (Cauchy)
     return xpx.apply_where(i, (x, a, b),
                            lambda x, a, b: Fi(x, a, b, a1=a1),
                            lambda x, a, b: 1 - Fi(-x, a, -b, a1=a1))[()]
@@ -108,7 +99,9 @@ def F(x, a, b):
 def f(x, a, b):
     x, a, b = xp_promote(x, a, b, force_floating=True, broadcast=True, xp=np)
     a1 = np.all(a == 1)
-    i = (x >= zetaa1(a, b)) if a1 else (x >= zeta(a, b))
+    i = x >= zeta(a, b, a1=a1)
+    # Avoid apply-where - change x and b
+    # ideally, a lazy-select function would let us handle a=1, b=0 (Cauchy)
     return xpx.apply_where(i, (x, a, b),
                            lambda x, a, b: fi(x, a, b, a1=a1),
                            lambda x, a, b: fi(-x, a, -b, a1=a1))[()]
