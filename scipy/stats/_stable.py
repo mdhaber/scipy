bf1c346bbbe7b371a81dffe21363b395934cd389
Matt Haberland <matt.haberland@gmail.com>
2026-01-24 23:54:07 -0800

    ENH: stats.Stable: improve PDF at a == 1

--- scipy/stats/_stable.py
+++ scipy/stats/_stable.py
@@ -3,6 +3,7 @@ from scipy import stats, special, integrate
 from scipy.optimize import elementwise
 import scipy._lib.array_api_extra as xpx
 from scipy._lib._array_api import xp_promote
+import matplotlib.pyplot as plt
 
 
 def zeta(a, b, *, a1):
@@ -14,7 +15,7 @@ def zeta(a, b, *, a1):
 
 def th0(a, b, *, a1):
     if not a1:
-        return 1/a * np.atan(b * np.tan(np.pi * a / 2))
+        return 1/a * np.atan(b * np.tan(np.pi * a / 2))  # unstable at np.pi/2
     else:
         return np.full_like(a, np.pi / 2)
 
@@ -31,7 +32,7 @@ def V(th, a, b, *, a1):
         th0_ = th0(a, b, a1=a1)
         t1 = (np.cos(a * th0_))**(1 / (a - 1))
         t2 = (np.cos(th) / np.sin(a * (th0_ + th)))**(a / (a - 1))
-        t3 = np.cos(a*th0_ + (a - 1)*th) / np.cos(th)
+        t3 = np.cos(a*th0_ + (a - 1)*th) / np.cos(th)  # unstable at np.pi/2
         return t1 * t2 * t3
     else:
         return 2 / np.pi * (np.pi/2 + b*th) / np.cos(th) * np.exp(
@@ -70,13 +71,15 @@ def Fi(x, a, b, *, a1):
 def fi(x, a, b, *, a1):
     def integrand(th, x, a, b):
         g_ = g(th, x, a, b, a1=a1)
+        # res[~(res >= 0)] = 0  # small effect at a == 1
         return g_ * np.exp(-g_)
 
     # re-evaluate nextafter on the right
-    bounds = (np.nextafter(-th0(a, b, a1=a1), np.inf), np.pi / 2)
+    bounds = (-th0(a, b, a1=a1), np.pi/2)
 
-    # root-finding doesn't work in the left tail (|b| ~ 1)
-    # th2 = elementwise.find_root(lambda th, x, a, b: g(th, x, a, b, a1=a1) - 1,
+    # This seems to be more trouble than it's worth - root finding doesn't work reliably
+    # when |b|=1 or a=2 when the roots are theoretically at interval endpoints
+    # th2 = elementwise.find_root(lambda th, x, a, b: g(th, x, a, b, a1=a1) - 1
     #                             bounds, args=(x, a, b))
     # integral1 = integrate.tanhsinh(integrand, bounds[0], th2.x, args=(x, a, b))
     # integral2 = integrate.tanhsinh(integrand, th2.x, np.pi/2, args=(x, a, b))
@@ -98,10 +101,10 @@ def F(x, a, b):
 
 def f(x, a, b):
     x, a, b = xp_promote(x, a, b, force_floating=True, broadcast=True, xp=np)
-    a1 = np.all(a == 1)
-    i = x >= zeta(a, b, a1=a1)
-    # Avoid apply-where - change x and b
-    # ideally, a lazy-select function would let us handle a=1, b=0 (Cauchy)
-    return xpx.apply_where(i, (x, a, b),
-                           lambda x, a, b: fi(x, a, b, a1=a1),
-                           lambda x, a, b: fi(-x, a, -b, a1=a1))[()]
+
+    i = x >= zeta(a, b, a1=False)
+    res = fi(np.where(i, x, -x), a, np.where(i, b, -b), a1=False)
+
+    a1 = (a == 1)
+    res[a1] = fi(x[a1], a[a1], b[a1], a1=True)
+    return res
