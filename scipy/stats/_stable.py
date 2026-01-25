8c0fe296c0d9dda77c3bc86873684c12fa65ae5b
Matt Haberland <matt.haberland@gmail.com>
2026-01-25 15:16:08 -0800

    ENH: Stable: polish pdf

--- scipy/stats/_stable.py
+++ scipy/stats/_stable.py
@@ -5,7 +5,14 @@ import scipy._lib.array_api_extra as xpx
 from scipy._lib._array_api import xp_promote
 import matplotlib.pyplot as plt
 
+# Stable Distribution
+# [1]: Nolan, John P. "Numerical calculation of stable densities and distribution
+#      functions." Communications in statistics. Stochastic models 13.4 (1997): 759-774.
+# [2]: Ament, Sebastian, and Michael O’Neil. "Accurate and efficient numerical
+#      calculation of stable densities via optimized quadrature and asymptotics."
+#      Statistics and Computing 28.1 (2018): 171-185.
 
+# [1], p. 761
 def zeta(a, b, *, a1):
     if not a1:
         return -b * np.tan(np.pi * a / 2)
@@ -39,6 +46,7 @@ def V(th, a, b, *, a1):
             1/b * (np.pi/2 + b*th) * np.tan(th))
 
 
+# # [1], p. 766, eq. (4)
 def c2(x, a, b, *, a1):
     if not a1:
         return a / (np.pi * np.abs(a - 1) * (x - zeta(a, b, a1=a1)))
@@ -64,32 +72,39 @@ def Fi(x, a, b, *, a1):
     def integrand(th, x, a, b):
         return np.exp(-g(th, x, a, b, a1=a1))
 
-    integral = integrate.tanhsinh(integrand, -th0(a, b, a1=a1), np.pi / 2, args=(x, a, b)).integral
+    bounds = (-th0(a, b, a1=a1), np.pi / 2)
+    integral = integrate.tanhsinh(integrand, *bounds, args=(x, a, b)).integral
     return c1(a, b, a1=a1) + c3(a, a1=a1) * integral
 
 
 def fi(x, a, b, *, a1):
     def integrand(th, x, a, b):
         g_ = g(th, x, a, b, a1=a1)
-        res = g_ * np.exp(-g_)
-        res[~(res >= 0)] = 0  # small effect at a == 1
+        res = np.asarray(g_ * np.exp(-g_))
+        # [1], pp. 766-767 "is continuous, positive, strictly monotonic... Thus g has
+        # the same properties..." But this is not true numerically. Experimentally,
+        # when the result is negative or NaN, it should have been ~0.
+        res[(res < 0) | np.isnan(res)] = 0
         return res
-        # return g_ * np.exp(-g_)
 
-    # re-evaluate nextafter on the right
     bounds = (-th0(a, b, a1=a1), np.pi/2)
 
-    # This seems to be more trouble than it's worth - root finding doesn't work reliably
-    # when |b|=1 or a=2 when the roots are theoretically at interval endpoints
-    th2 = elementwise.find_root(lambda th, x, a, b: g(th, x, a, b, a1=a1) - 1,
-                                bounds, args=(x, a, b))
-    # th2 = elementwise.find_minimum(lambda th, x, a, b: -integrand(th, x, a, b),
-    #                                (bounds[0], (bounds[0]+bounds[1])/1, bounds[1]), args=(x, a, b))
-    integral1 = integrate.tanhsinh(integrand, bounds[0], th2.x, args=(x, a, b), minlevel=4, rtol=1e-12)
-    integral2 = integrate.tanhsinh(integrand, th2.x, np.pi/2, args=(x, a, b), rtol=1e-12)
+    # [1], p. 767. "...the integrand can be very peaked. To avoid this problem,
+    # the program locates the peak (by numerically finding $\theta_2$ where g == 1...
+    # The integral is then evaluated in two pieces."
+    th2_res = elementwise.find_root(lambda th, x, a, b: g(th, x, a, b, a1=a1) - 1,
+                                    bounds, args=(x, a, b))
+
+    # What the paper doesn't mention:
+    # In some cases, there may not be a (numerical) root in the inclusive interval,
+    # so "break" the integration at an endpoint. One integral is a no-op / returns zero.
+    th2 = np.where(th2_res.success, th2_res.x, np.pi/2)
+
+    # Note: can get substantially better accuracy by increasing `minlevel`.
+    integral1 = integrate.tanhsinh(integrand, bounds[0], th2, args=(x, a, b))
+    integral2 = integrate.tanhsinh(integrand, th2, bounds[1], args=(x, a, b))
     integral = integral1.integral + integral2.integral
 
-    # integral = integrate.tanhsinh(integrand, *bounds, args=(x, a, b)).integral
     return c2(x, a, b, a1=a1) * integral
 
 
@@ -97,7 +112,6 @@ def F(x, a, b):
     x, a, b = xp_promote(x, a, b, force_floating=True, broadcast=True, xp=np)
     a1 = np.all(a == 1)
     i = (b > 0) if a1 else (x >= zeta(a, b, a1=a1))
-    # ideally, a lazy-select function would let us handle a=1, b=0 (Cauchy)
     return xpx.apply_where(i, (x, a, b),
                            lambda x, a, b: Fi(x, a, b, a1=a1),
                            lambda x, a, b: 1 - Fi(-x, a, -b, a1=a1))[()]
@@ -106,53 +120,66 @@ def F(x, a, b):
 def f(x, a, b):
     x, a, b = xp_promote(x, a, b, force_floating=True, broadcast=True, xp=np)
 
+    # [1], p. 761, Thm. 1 (c)
     z = zeta(a, b, a1=False)
     i = x >= z
     x = np.where(i, x, -x)
     b = np.where(i, b, -b)
 
-    # Nolan
+    # First, assume a != 1, and just evaluate according to strategy in [1].
     res = fi(x, a, b, a1=False)
+    res = np.asarray(res)
 
-    # Expansion for |x - zeta| small
+    # Use asymptotic expansions for small and large |x - zeta| from [2],
+    # assuming a != 1.
     z = zeta(a, b, a1=False)
     rtol = 1e-14
     eps = rtol * res
     n = 30
+
+    # [2], eq. (2.24)
     B0 = (eps*a*np.pi*(1 + z**2)**((n + 1) / (2*a))
           * special.gamma(n + 1)/special.gamma((n + 1)/a))**(1/n)
-    k = np.arange(0, n+1)
-    k = np.reshape(k, (-1,) + (1,)*x.ndim)
+    # "We find that truncating (2.18) after the n-1 term is accurate to within eps
+    #  of the true value for all x satisfying..."
     i = np.abs(x - z) <= B0
+    # [2], eq. (2.18)
+    k = np.reshape(np.arange(0, n+1), (-1,) + (1,)*x.ndim)
     S0 = 1/(a*np.pi) * np.sum(special.gamma((k + 1)/a)/special.gamma(k + 1)
                               * (1 + z**2)**(-(k+1)/(2*a))
                               * np.sin((np.pi/2 + np.atan(z)/a)*(k+1)) * (x - z)**k, axis=0)
     res[i] = S0[i]
 
-    # Expansion for |x - zeta| large
-    rtol = 1e-14
-    eps = rtol * res
-    n = 30
+    # [2], eq. (2.29)
     B_inf = (a / (np.pi * eps) * (1 + z**2)**(n/2)
              * special.gamma(a * n) / special.gamma(n)) ** (1/(a*n - 1))
+    # "... the series (2.25) is accurate to precision eps for any x satisfying..."
     i = (np.abs(x - zeta(a, b, a1=False)) > B_inf)
-    k = np.arange(1, n)
-    k = np.reshape(k, (-1,) + (1,)*x.ndim)
+    # [2], eq. (2.25)
+    k = np.reshape(np.arange(1, n), (-1,) + (1,)*x.ndim)
     S_inf = a / np.pi * np.sum((-1)**(k+1) * special.gamma(a*k)/special.gamma(k)
                                * (1 + z**2)**(k/2) * np.sin((np.pi*a/2 - np.atan(z))*k)
                                * (x - z)**(-a*k-1), axis=0)
     res[i] = S_inf[i]
 
-    # a == 1 (all formulas special-cased)
+    # a == 1 also evaluated according to [1], but the integrand takes a different form
     a1 = (a == 1)
     res[a1] = fi(x[a1], a[a1], b[a1], a1=True)
 
-    # Normal
-    a2 = (a == 2)
-    res[a2] = 1/np.sqrt(4 * np.pi) * np.exp(-x[a2]**2/4)
+    # Cauchy distribution
+    cauchy = (a == 1) & (b == 0)
+    res[cauchy] = 1 / (np.pi * (1 + x[cauchy]**2))
 
-    # Cauchy
-    a1b0 = (a == 1) & (b == 0)
-    res[a1b0] = 1 / (np.pi * (1 + x[a1b0]**2))
+    # Normal distribution - already accurate, so not worth the time to special case
+    # normal = (a == 2)
+    # res[normal] = 1/np.sqrt(4 * np.pi) * np.exp(-x[normal]**2/4)
 
+    # Levy distribution - already accurate, and formula below adds singularity at x=0
+    # levy = (a == 0.5) & (b == 1)
+    # res[levy] = 1/np.sqrt(2 * np.pi) * np.exp(-1/(2*x[levy])) / x[levy]**1.5
+
+    # Known issues:
+    # Tails where a == 1, b != 0
+    # a close, but not equal to, 1
+    # Small a
     return res
